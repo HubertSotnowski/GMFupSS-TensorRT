e8fbe09e3a47948266601ea90c0c13dd5de732d1
Hubert Sontowski <72866544+HubertSotnowski@users.noreply.github.com>
2022-11-13 01:21:33 +0100

    change opset to 16

--- 1.py
+++ 1.py
@@ -22,7 +22,7 @@ torch.onnx.export(
     x,  # model input (or a tuple for multiple inputs)
     "cain-temp.onnx",  # where to save the model (can be a file or file-like object)
     export_params=True,  # store the trained parameter weights inside the model file
-    opset_version=17,  # the ONNX version to export the model to
+    opset_version=16,  # the ONNX version to export the model to
     do_constant_folding=True,  # whether to execute constant folding for optimization
     input_names=input_names,  # the model's input names
     output_names=output_names,
