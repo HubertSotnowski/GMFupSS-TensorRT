e9aae5f3b1c547e8747a99ff4c51fa0f90e5e49d
Hubert Sontowski <72866544+HubertSotnowski@users.noreply.github.com>
2022-11-13 13:39:37 +0100

    Revert "add padding"
    This reverts commit d1735a4699a8daa8c840957db90832ff4c14c17a.

--- test-model.py
+++ test-model.py
@@ -8,7 +8,7 @@ from PIL import Image
 model = Model()
 
 img1 = Image.open("demo/stacked.jpg")
-img1 = img1.resize((854*2, 480))
+img1 = img1.resize((128, 128))
 convert_tensor = transforms.ToTensor()
 import cv2
 
@@ -18,11 +18,16 @@ input_names = [
     "x.1",
 ]
 output_names = ["output_frame"]
+f1 = torch.rand((1, 3, 512, 512)) / 2
+f2 = torch.rand((1, 3, 512, 512))
 x = img1
 print(img1.shape)
+print("finished")
 out = x
+print("model")
 out = model(x)
-#print(x)
+print("data")
+print(x)
 out = out.squeeze(dim=0)
 out = out.detach().cpu().numpy().transpose(1, 2, 0)
 print(out.shape)
